0720d0cb4fc2647048c5c62f96c3939a5b719fc3
Sjoerd <113519405+DreojsNL@users.noreply.github.com>
2023-12-14 21:55:30 +0100

    Finished Gun and added Map editor

--- DoomClone/Assets/Script/Player.cs
+++ DoomClone/Assets/Script/Player.cs
@@ -10,13 +10,16 @@ public class Player : MonoBehaviour
     private float pitch = 0f;
     public int maxHealth = 100;
     public int currentHealth;
+    public bool isMoving;
+    private Rigidbody rb;
 
-
-  
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         currentHealth = maxHealth;
+
+        // Get the Rigidbody component
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -28,22 +31,26 @@ public class Player : MonoBehaviour
         // Sprinting check
         float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : speed;
 
-        Vector3 movement = new Vector3(horizontal, 0, vertical) * currentSpeed * Time.deltaTime;
-        transform.Translate(movement);
+        // Calculate movement direction
+        Vector3 movement = (transform.forward * vertical + transform.right * horizontal).normalized * currentSpeed;
+
+        // Apply force to the Rigidbody
+        rb.AddForce(movement);
+        isMoving = movement.magnitude > 0.1f;
 
         // Player rotation
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
 
         // Rotate the player around the Y-axis (yaw)
-        Vector3 rotation = new Vector3(0, mouseX, 0) * rotationSpeed * Time.deltaTime;
-        transform.Rotate(rotation);
+        transform.Rotate(Vector3.up * mouseX);
 
         // Rotate the camera around the X-axis (pitch)
-        pitch -= mouseY * rotationSpeed * Time.deltaTime;
+        pitch -= mouseY;
         pitch = Mathf.Clamp(pitch, -maxLookUp, maxLookDown);
         Camera.main.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
     }
+
     public void TakeDamage(int damageAmount)
     {
         currentHealth -= damageAmount;
--- DoomClone/Assets/Script/Weapons/Gun.cs
+++ DoomClone/Assets/Script/Weapons/Gun.cs
@@ -8,13 +8,17 @@ public class Gun : MonoBehaviour
     public float spreadAngle = 20f;
     public float fireRate = 1f;
     public float shotRange = 10f;
+    public float moveSpeed = 5f; // Adjust the movement speed here
     public Image gunImage;
     public Sprite[] gunSprites;
-
+    public GameObject hitPrefab; // Assign your prefab in the inspector
+    public float hitPrefabOffset = 0.01f; // Adjust the offset to prevent z-fighting
+    public Animator imageAnimation;
     private bool canShoot = true;
+    private bool isShooting = false; // Added variable
     public AudioSource audioSource;
     private Camera playerCamera;
-    public Animator imageAnimation;
+    public Player playerScript;
 
     private int currentSpriteIndex = 0;
     public float scrollSpeed = 0.1f; // Adjust the scrolling speed here
@@ -30,9 +34,20 @@ public class Gun : MonoBehaviour
         {
             StartCoroutine(Shoot());
         }
-    }
 
-   
+        // Check if the player is moving and play the SGSway animation accordingly
+        if (playerScript.isMoving == true)
+        {
+            imageAnimation.SetBool("IsMoving", true);
+        }
+        else
+        {
+            imageAnimation.SetBool("IsMoving", false);
+        }
+
+        // Set the isShooting parameter in the animation controller
+        imageAnimation.SetBool("IsShooting", isShooting);
+    }
 
     public void ScrollImages()
     {
@@ -56,6 +71,7 @@ public class Gun : MonoBehaviour
     IEnumerator Shoot()
     {
         canShoot = false;
+        isShooting = true; // Set isShooting to true when shooting starts
         audioSource.Play();
         ScrollImages();
         imageAnimation.Play("SGRecoil");
@@ -64,14 +80,26 @@ public class Gun : MonoBehaviour
         for (int i = 0; i < pellets; i++)
         {
             float currentSpread = Random.Range(-spreadAngle / 2, spreadAngle / 2);
+            float verticalSpread = Random.Range(-spreadAngle / 2, spreadAngle / 2);
+
+            // Calculate direction based on spread angle for both horizontal and vertical axes
             Vector3 direction = playerCamera.transform.forward;
-            direction = Quaternion.Euler(0, currentSpread, 0) * direction;
+            direction = Quaternion.Euler(verticalSpread, currentSpread, 0) * direction;
 
             RaycastHit hit;
             if (Physics.Raycast(playerCamera.transform.position, direction, out hit, shotRange))
             {
                 Debug.Log("Hit: " + hit.collider.gameObject.name);
                 Debug.DrawLine(playerCamera.transform.position, hit.point, Color.red, 0.1f);
+
+                // Offset the instantiation position to prevent z-fighting
+                Vector3 hitPointWithOffset = hit.point + hit.normal * hitPrefabOffset;
+
+                // Instantiate the prefab at the hit point with offset
+                if (hitPrefab != null)
+                {
+                    Instantiate(hitPrefab, hitPointWithOffset, Quaternion.identity);
+                }
             }
             else
             {
@@ -81,7 +109,9 @@ public class Gun : MonoBehaviour
 
         yield return new WaitForSeconds(1f / fireRate);
         canShoot = true;
+        isShooting = false; // Set isShooting to false when shooting is done
     }
+
     public void ResetImage()
     {
         imageAnimation.Play("SGIdel");
